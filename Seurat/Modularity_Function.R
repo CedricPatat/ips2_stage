817109c07886ca7f6f4ca3c3d268138e25a7bdbc
CedricPatat <cedric.patat@outlook.fr>
2021-07-15 12:15:07 +0200

    addition of Modularity_Function.R

---
+++ Seurat/Modularity_Function.R
@@ -0,0 +1,45 @@
+###############################################################################
+##################                Modularity                ###################
+###############################################################################
+
+# [SEURAT]
+
+# Description
+#
+# For each cell, the algorithm checks that the K neighbors belong to the same 
+# cluster as this cell (K is k.param given in Seurat FindNeighbors())
+# Calculation of the modularity of clustering.
+# INPUT 1: Seurat Object
+# OUTPUT: modularity score
+
+
+# Function
+
+modularity = function(object){
+  
+  neighbors_k = as.list(0)
+  for(i in 1:length(derybel_epidermis_seurat@meta.data[["barcodes"]])){
+    nn_cells = derybel_epidermis_seurat@graphs[["RNA_snn"]][which(derybel_epidermis_seurat@graphs[["RNA_snn"]][-i,i]!=0),i]
+    neighbors_k[[i]] = (names(sort(nn_cells, decreasing = T)[1:20]))
+  }
+  
+  clusfrom = derybel_epidermis_seurat@active.ident
+  
+  for(i in 1:length(neighbors_k)){
+    neighbors_k[[i]]= as.numeric(derybel_epidermis_seurat@active.ident[neighbors_k[[i]]])
+  }
+  
+  modularity_score = 0
+  
+  for (i in 1: length(neighbors_k)){
+    modularity_score = modularity_score + sum(clusfrom[i] == neighbors_k[[i]])
+  }
+  
+  modularity_score = 1-(modularity_score/(length(neighbors_k)*20))
+  
+  modularity_score
+}
+
+
+# Example
+modularity(derybel_epidermis_seurat)
