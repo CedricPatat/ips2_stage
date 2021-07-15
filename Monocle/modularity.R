25cda6a7b0bfc6b52c096586d452ca9f9b425a3b
CedricPatat <cedric.patat@outlook.fr>
2021-07-15 11:59:25 +0200

    Modularity.R update

--- Monocle/modularity.R
+++ Monocle/modularity.R
@@ -1,14 +1,21 @@
 ###############################################################################
 ##################                Modularity                ###################
 ###############################################################################
+
+# [MONOCLE 3]
+
+# Description
+
+# For each cell, the algorithm checks that the K neighbors belong to the same 
+# cluster as this cell (K given in Monocle 3 cluster_cells ())
 # Calculation of the modularity of clustering.
 # INPUT 1: cds as Monocle's cds 
 # INPUT 2: reduction_method used as red_met (do not forget quotation marks)
 # OUTPUT: modularity score
-# For each cell, the algorithm checks that the K neighbors belong to the same 
-# cluster as this cell (K given in Monocle 3 cluster_cells ())
+
 
 #Function
+
 modularity = function(cds, red_meth) {
   myclusters= clusters(cds, reduction_method = red_meth)
   from = cds@clusters@listData[[red_meth]][["cluster_result"]][["relations"]][["from"]]
